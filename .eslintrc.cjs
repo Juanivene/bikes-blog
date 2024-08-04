23c704f6040b41660011eb4314bff6ece97b7d4e
Ignacio Bockl <ignaciobockl96@gmail.com>
2024-08-04 11:46:09 -0300

    feat: added 'prettier' plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,7 +20,7 @@ module.exports = {
     sourceType: 'module', // Use ECMAScript modules.
     project: './tsconfig.json', // Specify the TypeScript configuration file.
   },
-  plugins: ['react', 'react-hooks', '@typescript-eslint'],
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
   rules: {
     'no-param-reassign': 'off',
     'import/prefer-default-export': 'off',
