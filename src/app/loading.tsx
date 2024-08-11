b10cc6932f1d19727159721b38b3ecb3c536e749
Ignacio Bockl <ignaciobockl96@gmail.com>
2024-08-11 12:41:31 -0300

    fix: eslint - require-jsdoc

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,5 +1,10 @@
 import LoadingPage from '@/components/Loading/LoadingPage';
 
+/**
+ * LoadingScreen component that renders the LoadingPage component.
+ *
+ * @returns {JSX.Element} The rendered LoadingScreen component.
+ */
 const LoadingScreen = () => {
   return <LoadingPage />;
 };
