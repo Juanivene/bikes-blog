e23468f8737f86b22098a9c47628664815e08c09
Ignacio Bockl <ignaciobockl96@gmail.com>
2024-08-26 20:08:42 -0300

    fix: eslint - require-jsdoc & @typescript-eslint/explicit-function-return-type

--- src/redux/hooks/hook.ts
+++ src/redux/hooks/hook.ts
@@ -2,6 +2,17 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 
 import type { AppDispatch, RootState } from '@/redux/store/store';
 
-// Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch = () => useDispatch<AppDispatch>();
+/**
+ * Custom hook to use the `dispatch` function from the Redux store.
+ *
+ * @returns {AppDispatch} The dispatch function from the Redux store.
+ */
+export const useAppDispatch = (): AppDispatch => useDispatch<AppDispatch>();
+
+/**
+ * Custom hook to use the `selector` function from the Redux store.
+ *
+ * @template T The type of the state returned by the selector.
+ * @returns {TypedUseSelectorHook<RootState>} The selector function from the Redux store, typed with `RootState`.
+ */
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
