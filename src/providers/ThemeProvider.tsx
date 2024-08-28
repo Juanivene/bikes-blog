38b7e0d1b0c0ccde47ecc6fbf720a70e866db02c
Ignacio Bockl <ignaciobockl96@gmail.com>
2024-08-28 19:02:33 -0300

    fix: eslint - require-jsdoc & @typescript-eslint/explicit-function-return-type

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -4,8 +4,22 @@ import { ThemeProvider as NextThemesProvider } from 'next-themes';
 
 import { type ThemeProviderProps } from 'next-themes/dist/types';
 
-const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
-};
+/**
+ * Custom ThemeProvider component that wraps the `next-themes` ThemeProvider.
+ *
+ * This component allows for easy integration of theme management in a Next.js application,
+ * providing the ability to switch between different themes, manage system theme preferences,
+ * and apply various theme-related configurations.
+ *
+ * @param {ThemeProviderProps} props - The properties passed to the ThemeProvider.
+ * @param {React.ReactNode} props.children - The child components that will be rendered inside the ThemeProvider.
+ * @returns {JSX.Element} The rendered ThemeProvider component.
+ */
+const ThemeProvider = ({
+  children,
+  ...props
+}: ThemeProviderProps): JSX.Element => (
+  <NextThemesProvider {...props}>{children}</NextThemesProvider>
+);
 
 export default ThemeProvider;
