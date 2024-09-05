12bb7f65b30b31d9d0a959672b0515f798a44c42
Ignacio Bockl <ignaciobockl96@gmail.com>
2024-09-05 20:35:12 -0300

    feat: 'sweetalert2' library replaced by 'sonner' to display messages

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -1,6 +1,5 @@
 import cn from './cn';
-import { customSwal } from './customSwal';
 import fetchFn from './fetchFn';
 import removeLineBreaks from './utils';
 
-export { cn, customSwal, fetchFn, removeLineBreaks };
+export { cn, fetchFn, removeLineBreaks };
