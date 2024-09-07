56929ed2c66026fcda9b2847a536b152ebdee338
Ignacio Bockl <ignaciobockl96@gmail.com>
2024-09-07 14:35:29 -0300

    feat: centralization of imports

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -1,5 +1,24 @@
 import cn from './cn';
+import {
+  ErrorToast,
+  InfoToast,
+  LoadingToast,
+  SuccessToast,
+  WarningToast,
+} from './customAlerts/toastComponentes';
+import { customToast, promiseToast } from './customAlerts/toastUtils';
 import fetchFn from './fetchFn';
 import removeLineBreaks from './utils';
 
-export { cn, fetchFn, removeLineBreaks };
+export {
+  cn,
+  customToast,
+  ErrorToast,
+  fetchFn,
+  InfoToast,
+  LoadingToast,
+  promiseToast,
+  removeLineBreaks,
+  SuccessToast,
+  WarningToast,
+};
