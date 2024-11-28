438b433a556daf9c17f76642d3d0bb312b5eb64b
Ignacio Bockl <lmartin@justucuman.gov.ar>
2024-11-28 20:04:34 -0300

    feat: installing and configuring vitest for testing

---
+++ vitest.setup.ts
@@ -0,0 +1,12 @@
+
+import { expect } from 'vitest'; // Asegúrate de que 'expect' esté disponible
+import '@testing-library/jest-dom'; // Extiende 'expect' con matchers de jest-dom
+
+// mediaQuery
+globalThis.matchMedia = globalThis.matchMedia || function() {
+    return {
+      matches: false,
+      addEventListener: () => {},
+      removeEventListener: () => {},
+    };
+  };
