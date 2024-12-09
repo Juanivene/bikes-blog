3a6d9a8f09ee400169b7a14c1c670fda62ffc19d
Ignacio Bockl <lmartin@justucuman.gov.ar>
2024-12-09 20:57:55 -0300

    test: 'Alert Component' - general rendering of the component

--- src/components/ui/components/Alert/Alert.test.tsx
+++ src/components/ui/components/Alert/Alert.test.tsx
@@ -3,15 +3,19 @@ import { render, screen } from '@testing-library/react';
 import Alert from './Alert';
 
 describe('Alert Component', () => {
-  it('snapshot', () => {
-    const { container } = render(<Alert>Default Alert</Alert>);
+  let container: HTMLElement;
 
+  beforeEach(() => {
+    // Renderizamos el componente una sola vez antes de cada prueba
+    const renderResult = render(<Alert>Default Alert</Alert>);
+    container = renderResult.container;
+  });
+
+  it('snapshot', () => {
     expect(container).toMatchSnapshot();
   });
 
   it('renders the alert with default props', () => {
-    render(<Alert>Default Alert</Alert>);
-
     const alert = screen.getByText('Default Alert');
     expect(alert).toBeInTheDocument();
   });
