4005296c61e1a4e154e6e8d3f552c838fa42bb2d
Ignacio Bockl <lmartin@justucuman.gov.ar>
2024-12-09 20:53:47 -0300

    test: 'Alert Component' - snapshot

--- src/components/ui/components/Alert/Alert.test.tsx
+++ src/components/ui/components/Alert/Alert.test.tsx
@@ -3,6 +3,12 @@ import { render, screen } from '@testing-library/react';
 import Alert from './Alert';
 
 describe('Alert Component', () => {
+  it('snapshot', () => {
+    const { container } = render(<Alert>Default Alert</Alert>);
+
+    expect(container).toMatchSnapshot();
+  });
+
   it('renders the alert with default props', () => {
     render(<Alert>Default Alert</Alert>);
 
