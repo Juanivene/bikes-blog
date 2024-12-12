daba2704dc56b2931cfb43139dfc6619e03f36e3
juaniveneziano <168385555+Juanivene@users.noreply.github.com>
2024-12-12 07:35:26 -0300

    fix: dont show id in card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,31 @@
-const Home = (): React.ReactElement => <main>Inico</main>;
+import BikeCard from '@/components/ui/components/BikeCard/BikeCard';
 
-export default Home;
+type Bikes = {
+  id: number;
+  title: string;
+  description: string;
+  isFavorite: boolean;
+};
+
+const BikesList = async (): Promise<React.ReactElement> => {
+  const response = await fetch('http://localhost:4000/bikes');
+  const bikes = (await response.json()) as Bikes[];
+
+  return (
+    <div>
+      <h1>Lista de Motos</h1>
+      <ul>
+        {bikes.map((bike) => (
+          <BikeCard
+            isFavorite
+            description={bike.description}
+            key={bike.id}
+            title={bike.title}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default BikesList;
