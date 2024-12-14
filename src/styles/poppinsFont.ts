87ed2940236d2f6035d647ef95659da39236e7e6
Juanivene <168385555+Juanivene@users.noreply.github.com>
2024-12-14 20:23:29 -0300

    feat: modal component with detail of the bike and handle select markdown file

---
+++ src/styles/poppinsFont.ts
@@ -0,0 +1,8 @@
+import { Poppins as poppinsFont } from 'next/font/google';
+
+const poppins = poppinsFont({
+  weight: '400',
+  style: 'normal',
+  subsets: ['latin'],
+});
+export default poppins;
